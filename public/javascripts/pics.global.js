58adef42cdf54dc34cda4d541b3043da441532aa
r79 <r@r79.io>
2015-06-27 18:03:02 +0200

    Initial Commit Does not contain .idea and no node-modules, does contain js-libs though untill I added  bower support

---
+++ public/javascripts/pics.global.js
@@ -0,0 +1,8 @@
+/**
+ * Created by r79 on 6/27/2015.
+ */
+var app = angular.module('picsGlobal', []);
+
+app.controller('mainController', function ($scope) {
+    $scope.test = "It works with angular! And NodeJS!"
+});
