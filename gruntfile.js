01d30350daa9ab6dfceb2f5a0fbf79c91baa9d54
r79 <r@r79.io>
2015-07-06 23:27:54 +0200

    once again a Backup commit + wiredep is working, still does not include the pics.global.js

--- gruntfile.js
+++ gruntfile.js
@@ -4,18 +4,9 @@ module.exports = function (grunt) {
     grunt.initConfig({
         wiredep: {
             task: {
-                // Point to the files that should be updated when
-                // you run `grunt wiredep`
                 src: [
-                    'views/**/*.jade',   // .jade support...
-                ],
-
-                options: {
-                    // See wiredep's configuration documentation for the options
-                    // you may pass:
-
-                    // https://github.com/taptapship/wiredep#configuration
-                }
+                    'views/*.jade'
+                ]
             }
         }
     });
