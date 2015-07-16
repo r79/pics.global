0d2c07584e4935640a4084cfaabc82e0e8e8eb92
Raphael Büchler <raphael.buechler@netcetera.com>
2015-07-16 10:58:25 +0200

    version 0.2.0
    + fixed imagecycle, shows last pic forever now
    + fixed backup
    + added disclaimer

--- ressources/javascripts/picsglobal.js
+++ ressources/javascripts/picsglobal.js
@@ -1,9 +1,5 @@
 var app = angular.module('picsGlobal', ['ngFileUpload'])
 
-    .controller('sampleTextController', function ($scope, Upload) {
-        $scope.test = "It works with angular! And NodeJS!";
-    })
-
     .controller('imageUploadController', function ($scope, Upload) {
         $scope.$watch('files', function () {
             $scope.upload($scope.files);
@@ -21,14 +17,14 @@ var app = angular.module('picsGlobal', ['ngFileUpload'])
                     }).progress(function (evt) {
                         var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
                     }).success(function (data, status, headers, config) {
-                        
+
                     });
                 }
             }
         };
     })
 
-    .controller('canvasController', function($http) {
+    .controller('canvasController', function ($http) {
         var canvas = angular.element(currentImage)[0];
         var ctx = canvas.getContext('2d');
         var socket = io.connect();
@@ -41,15 +37,15 @@ var app = angular.module('picsGlobal', ['ngFileUpload'])
                 drawImageScaled(this, ctx)
             };
 
-            img.src = './image/current?thisAttributeIsOnlyToLetAllBrowsersIgnoreCacheAndDoesNothingFunctionalAtAllOtherwise=' + Date.now();
             img.onerror = function () {
                 console.log('error occured');
                 this.src = './ressources/noimges.png';
-            }
+            };
+
+            img.src = './image/current?thisAttributeIsOnlyToLetAllBrowsersIgnoreCacheAndDoesNothingFunctionalAtAllOtherwise=' + Date.now();
         };
 
         drawNewImage();
-
         socket.on('imageCycle', function () {
             drawNewImage();
         });
@@ -57,14 +53,14 @@ var app = angular.module('picsGlobal', ['ngFileUpload'])
         //I TL;DR copy pasted this.
         // thx to GameAlchemist@Stackoverflow: http://stackoverflow.com/a/23105310
         function drawImageScaled(img, ctx) {
-            var canvas = ctx.canvas ;
-            var hRatio = canvas.width  / img.width    ;
-            var vRatio =  canvas.height / img.height  ;
-            var ratio  = Math.min ( hRatio, vRatio );
-            var centerShift_x = ( canvas.width - img.width*ratio ) / 2;
-            var centerShift_y = ( canvas.height - img.height*ratio ) / 2;
-            ctx.clearRect(0,0,canvas.width, canvas.height);
-            ctx.drawImage(img, 0,0, img.width, img.height,
-                centerShift_x,centerShift_y,img.width*ratio, img.height*ratio);
+            var canvas = ctx.canvas;
+            var hRatio = canvas.width / img.width;
+            var vRatio = canvas.height / img.height;
+            var ratio = Math.min(hRatio, vRatio);
+            var centerShift_x = ( canvas.width - img.width * ratio ) / 2;
+            var centerShift_y = ( canvas.height - img.height * ratio ) / 2;
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.drawImage(img, 0, 0, img.width, img.height,
+                centerShift_x, centerShift_y, img.width * ratio, img.height * ratio);
         }
     });
--- services/imageQueueService.js
+++ services/imageQueueService.js
@@ -1,25 +1,29 @@
+//npm modules
 var fs = require('fs');
 var streamifier = require('streamifier');
 
-var imageQueue = [];
+//project modules
 var imageController = require('../routes/image.js');
 var webSocketService = require('../services/webSocketService.js');
-var nextImageInterval;
 
-var currentImage;
-
+//static variables
 var UPLOAD_FOLDER = './hiddenFileUploads/';
 var BACKUP_FILE_PATH = './backupImageQueue';
 
+//runtime variables
+var cycle;
+var imageQueue = [];
+var currentImage;
 
-var putImageInQueue = function(imageObject){
+//-------------------------------------------
+
+var putImageInQueue = function (imageObject) {
     var imageName = Date.now();
     var outputFileStream = fs.createWriteStream(UPLOAD_FOLDER + imageName);
     streamifier.createReadStream(imageObject.buffer).pipe(outputFileStream);
-    imageQueue.push(imageName);
-    if(!nextImageInterval) {
+    imageQueue.push(imageName.toString());
+    if(!cycle) {
         nextImage();
-        nextImageInterval = setInterval(nextImage, 13370);
     }
 };
 
@@ -28,45 +32,45 @@ var getCurrentImageReadStream = function () {
 };
 
 var nextImage = function () {
-    queueBackup();
-    console.log('new image interval');
-    var topOfQueue = imageQueue.shift();
-    if(!topOfQueue) {
-        console.log('queue is empty!');
-        clearInterval(nextImageInterval);
-        nextImageInterval = undefined;
-        currentImage = undefined;
-        return;
+    console.log('cycling images...');
+
+    if (imageQueue.length <= 1) {
+        console.log('queue is empty, keeping current image');
+        cycle = undefined;
+    } else {
+        fs.unlink(UPLOAD_FOLDER + imageQueue.shift());
+        cycle = setTimeout(nextImage, 13370);
     }
-    if(currentImage) {
-        fs.unlink(UPLOAD_FOLDER + currentImage);
-    }
-    currentImage = topOfQueue;
+    currentImage = imageQueue[0];
     webSocketService.sendImageCycle();
+    queueBackup();
 };
 
-var queueBackup = function() {
+var queueBackup = function () {
     console.log('created Queue Backup' + JSON.stringify(imageQueue));
     var backupFileStream = fs.createWriteStream(BACKUP_FILE_PATH);
     streamifier.createReadStream(JSON.stringify(imageQueue)).pipe(backupFileStream);
 };
 
-
 //Constructor
-(function() {
-    if(fs.existsSync(BACKUP_FILE_PATH)){
-        imageQueue = JSON.parse(fs.readFileSync(BACKUP_FILE_PATH));
-        console.log('restored previous state');
-        var files =fs.readdirSync(UPLOAD_FOLDER);
-        files.forEach(function (file) {
-            if(imageQueue.indexOf(file)===-1){
-                fs.unlink(UPLOAD_FOLDER + file);
-            }
-        });
-        console.log('cleaned up');
-        nextImageInterval = setInterval(nextImage, 13370);
-        nextImage();
+(function () {
+    if (fs.existsSync(UPLOAD_FOLDER)) {
+        if (fs.existsSync(BACKUP_FILE_PATH)) {
+            imageQueue = JSON.parse(fs.readFileSync(BACKUP_FILE_PATH));
+            console.log('restored previous state');
+            var files = fs.readdirSync(UPLOAD_FOLDER);
+            files.forEach(function (file) {
+                if (imageQueue.indexOf(file) === -1) {
+                    fs.unlink(UPLOAD_FOLDER + file);
+                }
+            });
+            console.log('cleaned up');
+        }
+    } else {
+        fs.mkdir(UPLOAD_FOLDER);
     }
+
+    nextImage();
 })();
 
 exports.putImageInQueue = putImageInQueue;
